f92744f69cf7648fe3cd8b2e83f73c6d7ef55e7a
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-16 19:27:08 +0530

    add webpack html plugin

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,6 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   devServer: {
     port: "8081",
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };

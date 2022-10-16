7b7fb709065e869d20abc207451e4885bfa2b5fd
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-16 19:46:07 +0530

    initialize container app

---
+++ contianer/webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: "8080",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};

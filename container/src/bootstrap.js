2867f4cfb2370232af945b88b02786bd5aea6ad6
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-10-24 20:32:41 +0530

    refactor container to reflect sub-app execution contexts

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,7 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as mountProducts } from "products/ProductsIndex";
+import { mount as mountCart } from "cart/CartShow";
 console.log("Container..!");
+
+mountProducts(document.querySelector("#products-container"));
+
+mountCart(document.querySelector("#cart-container"));
